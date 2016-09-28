cd1ef64d6b160a04fd6956f82d3d99c90b8957bd
James Martin <jmartin9725695@hotmail.com>
2016-09-28 15:26:49 +1000

    JM - Test commit

--- src/broker/protocols/BrokerAgent.java
+++ src/broker/protocols/BrokerAgent.java
@@ -38,7 +38,8 @@ import jade.domain.FIPAAgentManagement.FailureException;
 import java.util.Vector;
 
 
- // this is a test comment for github 
+// I changed this test comment for github
+// new comment
 /**
    This example shows how to implement nested protocols.
    In this case in particular we want to implement a broker agent
