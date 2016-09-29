e9cbaeb5e4acc1a4bf54c6182c6a37cff0a79bb4
James Martin <9725695@student.swin.edu.au>
2016-09-29 13:11:13 +1000

    JM - Recommit

---
+++ src/retailer/protocols/RetailerAgent.java
@@ -0,0 +1,43 @@
+package retailer.protocols;
+
+import jade.core.Agent;
+import jade.domain.FIPANames;
+import jade.domain.FIPAAgentManagement.FailureException;
+import jade.domain.FIPAAgentManagement.NotUnderstoodException;
+import jade.domain.FIPAAgentManagement.RefuseException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.AchieveREResponder;
+
+
+public class RetailerAgent extends Agent {
+	private float availableEnergy;
+	private float pricePerUnit;
+	
+	protected void setup() {
+		System.out.println("Agent "+getLocalName()+" waiting for requests...");
+	  	MessageTemplate template = MessageTemplate.and(
+	  		MessageTemplate.MatchProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST),
+	  		MessageTemplate.MatchPerformative(ACLMessage.REQUEST) );
+	  		
+		addBehaviour(new AchieveREResponder(this, template) {
+			protected ACLMessage prepareResponse(ACLMessage request) throws NotUnderstoodException, RefuseException {
+				System.out.println("Agent "+getLocalName()+": REQUEST received from "+request.getSender().getName()+". Action is "+request.getContent());
+				// We agree to perform the action. Note that in the FIPA-Request
+				// protocol the AGREE message is optional. Return null if you
+				// don't want to send it.
+				System.out.println("Agent "+getLocalName()+": Agree");
+				ACLMessage agree = request.createReply();
+				agree.setPerformative(ACLMessage.AGREE);
+				return agree;
+			}
+			
+			protected ACLMessage prepareResultNotification(ACLMessage request, ACLMessage response) throws FailureException {
+				System.out.println("Agent "+getLocalName()+": Action successfully performed");
+				ACLMessage inform = request.createReply();
+				inform.setPerformative(ACLMessage.INFORM);
+				return inform;
+			}
+		} );
+	}
+}
