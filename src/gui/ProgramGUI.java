b0b41e19ce4969f9a014fad39b921b684a82c72f
James Martin <9725695@student.swin.edu.au>
2016-11-01 18:07:58 +1100

    NEW: ProgramGUI wireframe complete for agent statuses.

--- src/gui/ProgramGUI.java
+++ src/gui/ProgramGUI.java
@@ -76,15 +76,109 @@ public class ProgramGUI extends JFrame implements SupplierVocabulary {
 		gbc_lMessages.gridy = 1;
 		pBody.add(lMessages, gbc_lMessages);
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
-		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.gridheight = 2;
-		gbc_panel.insets = new Insets(0, 0, 5, 5);
-		gbc_panel.fill = GridBagConstraints.BOTH;
-		gbc_panel.gridx = 1;
-		gbc_panel.gridy = 2;
-		pBody.add(panel, gbc_panel);
+		JPanel pAgents = new JPanel();
+		pAgents.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		GridBagConstraints gbc_pAgents = new GridBagConstraints();
+		gbc_pAgents.gridheight = 2;
+		gbc_pAgents.insets = new Insets(0, 0, 5, 5);
+		gbc_pAgents.fill = GridBagConstraints.BOTH;
+		gbc_pAgents.gridx = 1;
+		gbc_pAgents.gridy = 2;
+		pBody.add(pAgents, gbc_pAgents);
+		GridBagLayout gbl_pAgents = new GridBagLayout();
+		gbl_pAgents.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_pAgents.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_pAgents.columnWeights = new double[]{1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_pAgents.rowWeights = new double[]{1.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		pAgents.setLayout(gbl_pAgents);
+		
+		JPanel pAppliance1 = new JPanel();
+		pAppliance1.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		GridBagConstraints gbc_pAppliance1 = new GridBagConstraints();
+		gbc_pAppliance1.insets = new Insets(0, 0, 5, 5);
+		gbc_pAppliance1.weighty = 25.0;
+		gbc_pAppliance1.weightx = 20.0;
+		gbc_pAppliance1.fill = GridBagConstraints.BOTH;
+		gbc_pAppliance1.gridx = 1;
+		gbc_pAppliance1.gridy = 1;
+		pAgents.add(pAppliance1, gbc_pAppliance1);
+		
+		JPanel pAppliance2 = new JPanel();
+		pAppliance2.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		GridBagConstraints gbc_pAppliance2 = new GridBagConstraints();
+		gbc_pAppliance2.weighty = 25.0;
+		gbc_pAppliance2.weightx = 20.0;
+		gbc_pAppliance2.insets = new Insets(0, 0, 5, 5);
+		gbc_pAppliance2.fill = GridBagConstraints.BOTH;
+		gbc_pAppliance2.gridx = 1;
+		gbc_pAppliance2.gridy = 3;
+		pAgents.add(pAppliance2, gbc_pAppliance2);
+		
+		JPanel pAppliance3 = new JPanel();
+		pAppliance3.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		GridBagConstraints gbc_pAppliance3 = new GridBagConstraints();
+		gbc_pAppliance3.weighty = 25.0;
+		gbc_pAppliance3.weightx = 20.0;
+		gbc_pAppliance3.insets = new Insets(0, 0, 5, 5);
+		gbc_pAppliance3.fill = GridBagConstraints.BOTH;
+		gbc_pAppliance3.gridx = 1;
+		gbc_pAppliance3.gridy = 5;
+		pAgents.add(pAppliance3, gbc_pAppliance3);
+		
+		JPanel pHome = new JPanel();
+		pHome.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		GridBagConstraints gbc_pHome = new GridBagConstraints();
+		gbc_pHome.weighty = 25.0;
+		gbc_pHome.weightx = 20.0;
+		gbc_pHome.insets = new Insets(0, 0, 5, 5);
+		gbc_pHome.fill = GridBagConstraints.BOTH;
+		gbc_pHome.gridx = 3;
+		gbc_pHome.gridy = 3;
+		pAgents.add(pHome, gbc_pHome);
+		
+		JPanel pBroker = new JPanel();
+		pBroker.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		GridBagConstraints gbc_pBroker = new GridBagConstraints();
+		gbc_pBroker.weighty = 25.0;
+		gbc_pBroker.weightx = 20.0;
+		gbc_pBroker.insets = new Insets(0, 0, 5, 5);
+		gbc_pBroker.fill = GridBagConstraints.BOTH;
+		gbc_pBroker.gridx = 5;
+		gbc_pBroker.gridy = 3;
+		pAgents.add(pBroker, gbc_pBroker);
+		
+		JPanel pRetailer1 = new JPanel();
+		pRetailer1.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		GridBagConstraints gbc_pRetailer1 = new GridBagConstraints();
+		gbc_pRetailer1.weighty = 25.0;
+		gbc_pRetailer1.weightx = 20.0;
+		gbc_pRetailer1.insets = new Insets(0, 0, 5, 5);
+		gbc_pRetailer1.fill = GridBagConstraints.BOTH;
+		gbc_pRetailer1.gridx = 7;
+		gbc_pRetailer1.gridy = 1;
+		pAgents.add(pRetailer1, gbc_pRetailer1);
+		
+		JPanel pRetailer2 = new JPanel();
+		pRetailer2.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		GridBagConstraints gbc_pRetailer2 = new GridBagConstraints();
+		gbc_pRetailer2.weighty = 25.0;
+		gbc_pRetailer2.weightx = 20.0;
+		gbc_pRetailer2.insets = new Insets(0, 0, 5, 5);
+		gbc_pRetailer2.fill = GridBagConstraints.BOTH;
+		gbc_pRetailer2.gridx = 7;
+		gbc_pRetailer2.gridy = 3;
+		pAgents.add(pRetailer2, gbc_pRetailer2);
+		
+		JPanel pRetailer3 = new JPanel();
+		pRetailer3.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		GridBagConstraints gbc_pRetailer3 = new GridBagConstraints();
+		gbc_pRetailer3.weighty = 25.0;
+		gbc_pRetailer3.weightx = 20.0;
+		gbc_pRetailer3.insets = new Insets(0, 0, 5, 5);
+		gbc_pRetailer3.fill = GridBagConstraints.BOTH;
+		gbc_pRetailer3.gridx = 7;
+		gbc_pRetailer3.gridy = 5;
+		pAgents.add(pRetailer3, gbc_pRetailer3);
 		
 		JTextArea textArea = new JTextArea("Test");
 		textArea.setEnabled(false);
